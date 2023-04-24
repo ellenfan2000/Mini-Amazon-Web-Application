87ed704b763f7e84af65c952d80578d86a799924
Ellen Fan <rf96@duke.edu>
2023-04-24 17:50:04 -0400

    modify structure

---
+++ MiniAmazon/amazon/backend/database.py
@@ -0,0 +1,82 @@
+import sqlalchemy
+import math
+import struct
+from sqlalchemy.orm import sessionmaker, mapper
+from datetime import datetime
+from sqlalchemy.engine import URL
+from sqlalchemy import Table, create_engine, MetaData, text, inspect
+from sqlalchemy import Column, Integer, Text, Float, ForeignKey, String, CheckConstraint, LargeBinary
+from sqlalchemy import Enum, Float, PrimaryKeyConstraint, DateTime, func
+from sqlalchemy.orm import relationship, declarative_base, subqueryload
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'auth_user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(150), unique=True)
+    password = Column(String(128))
+    email = Column(String(254), unique=True)
+    first_name = Column(String(30))
+    last_name = Column(String(150))
+    is_staff = Column(Integer)
+    is_active = Column(Integer)
+    date_joined = Column(String(40))
+
+class Warehouse(Base):
+    __tablename__ ="warehouse"
+
+    id = Column(Integer, primary_key = True, autoincrement=True)
+    x = Column(Integer, nullable=False)
+    y = Column(Integer, nullable=False)
+
+
+class Products(Base):
+    __tablename__ = "product"
+    
+    id = Column(Integer, primary_key = True, autoincrement=True)
+    name = Column(Text, nullable=False)
+    description = Column(Text, nullable = True)
+    # picture = Column(LargeBinary)
+    price = Column(Float)
+    category = Column(Text, nullable = True)
+    inventory = Column(Integer)
+    warehouse = Column(Integer, ForeignKey('warehouse.id'))
+    
+class Package(Base):
+    __tablename__ = "package"
+
+    id = Column(Integer, primary_key = True, autoincrement=True)
+    packageID = Column(Integer, unique=True)
+    warehouse = Column(Integer, ForeignKey('warehouse.id'))
+    address_x = Column(Integer, nullable=False)
+    address_y = Column(Integer, nullable=False)
+
+class Order(Base):
+    __tablename__ = "order"
+
+    id = Column(Integer, primary_key = True, autoincrement=True)
+    buyer = Column(Integer, ForeignKey('auth_user.id'))
+    product = Column(Integer, ForeignKey('product.id'))
+    status = Column(String, CheckConstraint("status IN ('packing', 'packed', 'loading', 'loaded', 'delivering', 'delivered')"))
+    package = Column(Integer, ForeignKey('package.id'))
+    rate = Column(Integer, nullable=True)
+    comment = Column(Text)
+
+def initDataBase():
+    # db_url = f"postgresql+psycopg2://postgres:passw0rd@db:5432/Amazon"
+    db_url = f"postgresql+psycopg2://postgres:passw0rd@127.0.0.1:5432/Amazon"
+    engine = create_engine(db_url)
+    Base.metadata.drop_all(engine, [Warehouse.__table__, Products.__table__, Package.__table__, Order.__table__])
+    Base.metadata.create_all(engine, checkfirst=True)
+    
+    return engine
+
+def getEngine():
+    # db_url = f"postgresql+psycopg2://postgres:passw0rd@db:5432/Amazon"
+    db_url = f"postgresql+psycopg2://postgres:passw0rd@127.0.0.1:5432/Amazon"
+    engine = create_engine(db_url)
+
+    return engine
+    
---
+++ MiniAmazon/amazon/backend/query.py
@@ -0,0 +1,18 @@
+import WorldMessage
+import UPSMessage
+
+def get_recommend():
+    pass
+
+def get_search_res(user_input):
+    pass
+
+def get_product_detail(product_id):
+    pass
+
+def get_all_orders(user_id):
+    pass
+
+def get_order_details(user_id, order_id):
+    pass
+
