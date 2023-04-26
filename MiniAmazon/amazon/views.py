53f85d0d6184764630ad43333464366428dec6fe
Xinmeng <xc139@duke.edu>
2023-04-26 02:00:43 -0400

    can add comments and changed package_id way

--- MiniAmazon/amazon/views.py
+++ MiniAmazon/amazon/views.py
@@ -93,17 +93,31 @@ def product_details(request, id):
             try:
                 buy_product(request.user.id, id, form.cleaned_data["amount"], (
                     form.cleaned_data["address_x"], form.cleaned_data["address_y"]))
-            except:
-                messages.error(request, "The operation is invalid, please try again!")
+                return redirect("/order_details/"+str(id))
+            except Exception as e:
+                messages.error(request, e)
     form = BuyForm()
     return render(request, "Amazon/product_details.html", {"details": details,"form":form})
 
 @login_required(login_url='/login/')
 def my_orders(request):
     orders = get_all_orders(request.user.id)
-    return render(request,  "Amazon/my_orders.html",{"orders":orders})
+    res=[{"name":o.product.name,"amount":o.amount,"cost":round(o.amount*o.product.price,2),
+          "status":o.status,"id":o.id} for o in orders]
+
+    return render(request,  "Amazon/my_orders.html",{"orders":res})
 
 @login_required(login_url='/login/')
 def order_details(request,id):
-    order = get_product_detail(id)
-    return render(request,  "Amazon/order_details.html",{"details":order})
+    order = get_order_details(id)
+    cost=round(order.Order.amount*order.Order.product.price,2)
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            try:                  
+                set_comments(request.user.id, id, form.cleaned_data["rate"], form.cleaned_data["comment"] )
+                return redirect("/order_details/"+str(id))
+            except:
+                messages.error(request, "Cannot save the change, please try again!")                
+    form = FeedbackForm()
+    return render(request,  "Amazon/order_details.html",{"details":order,"cost":cost,"form":form})
