96267072082acb33a097a4db057ec924cbab4fb7
Ellen Fan <rf96@duke.edu>
2023-04-29 16:29:36 -0400

    docker work

--- MiniAmazon/amazon/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-# sys.path.append(os.path.dirname(SCRIPT_DIR))
-# print("ohho")
